667d370bdeaea02cc322d12a71b74749376d6e2f
KauztoKuro <ikkikurogane70@gmail.com>
2022-06-21 15:27:44 +0630

    key feature

--- next.config.js
+++ next.config.js
@@ -3,8 +3,34 @@ const optimizedImages = require("next-optimized-images");
 
 module.exports = withPlugins([optimizedImages], { target: "serverless" });
 
+// module.exports = {
+//   images: {
+//     disableStaticImages: true,
+//   },
+// };
+
 module.exports = {
-  assets: {
-    disableStaticImages: true,
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+};
+
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
   },
 };
